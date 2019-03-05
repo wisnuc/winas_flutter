f7125707f149fbe79b69c081bbc4a87d40647461
lixinwei <xinwei.li@winsuntech.cn>
2019-03-05 17:08:24 +0800

    fix bug of wechat login in ios

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,5 +1,7 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+#import <WXApi.h>
+#import <FluwxResponseHandler.h>
 
 @implementation AppDelegate
 
@@ -10,4 +12,11 @@
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
-@end
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    return [WXApi handleOpenURL:url delegate:[FluwxResponseHandler defaultManager]];
+}
+// NOTE: 9.0以后使用新API接口
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString*, id> *)options
+{
+    return [WXApi handleOpenURL:url delegate:[FluwxResponseHandler defaultManager]];
+}@end
